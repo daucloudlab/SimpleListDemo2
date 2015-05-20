011d81bf91d11c3e2027dd78833cc99df22c94db
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-20 14:36:16 +0600

    version 01

---
+++ app/src/main/java/kz/abcsoft/simplelistdemo2/MainActivity.java
@@ -0,0 +1,65 @@
+package kz.abcsoft.simplelistdemo2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    final String LOG_TAG = "myLogs";
+
+    ListView lvMain;
+    String[] names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvMain = (ListView)findViewById(R.id.lvMain) ;
+        lvMain.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.names,
+                android.R.layout.simple_list_item_single_choice) ;
+        lvMain.setAdapter(adapter);
+
+        Button btnChecked = (Button) findViewById(R.id.btnChecked);
+        btnChecked.setOnClickListener(this);
+
+        names = getResources().getStringArray(R.array.names);
+
+    }
+
+    public void onClick(View arg0) {
+        Log.d(LOG_TAG, "checked: " + names[lvMain.getCheckedItemPosition()]);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
